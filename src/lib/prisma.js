a7876ef9601380c3da6a77b8399f80a4edf327b9
Piyush-13090 <piyushsharma130906@gmail.com>
2025-12-07 23:49:34 +0530

    done

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,10 @@
-import {PrismaClient} from "@/generated/prisma/index"
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
+dotenv.config();
 
-const prisma = new PrismaClient()
+const connectionString = process.env.DATABASE_URL;
 
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 export default prisma
